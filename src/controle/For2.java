436f9b61a5d108f019aec2030fe108df4266add7
LucasDeIuliis <lucas_julio99@hotmail.com>
2021-07-08 18:14:24 -0300

    adicionei a bin

--- src/controle/For2.java
+++ src/controle/For2.java
@@ -3,7 +3,7 @@ package controle;
 public class For2 {
 
 	public static void main(String[] args) {
-		
+		 //teste
 		
 		for (int contador = 10; contador >= 0 ; contador -=2 ) {
 			System.out.printf("i=%d\n", contador);
